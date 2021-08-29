9c26f7123326355354a24e9f76d10cf3eba85d5e
Vikky <vikkyhp0007@gmail.com>
2021-08-29 18:11:00 +0530

    Auth0-Authentication

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Title from "./components/Title";
+import UploadForm from "./components/UploadForm";
+function App() {
+  return (
+    <div className="App">
+      <Title />
+      <UploadForm />
+    </div>
+  );
+}
+
+export default App;
